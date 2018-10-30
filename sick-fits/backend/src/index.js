758cffc8196899117f7906b1fc65c2706693437d
Michael Flis <michaelflis07@gmail.com>
2018-10-30 12:09:24 -0700

    adds currently logged in user w/ middleware and render props

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,4 +1,6 @@
 const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
@@ -8,6 +10,17 @@ const server = createServer();
 // express middleware to handle cookies
 server.express.use(cookieParser())
 // TODO use express middleware to populate current user
+// decode the JWT so we can get user id on each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if ( token ) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put the user id onto the req for future requests
+    req.userId = userId;
+  }
+  next();
+})
+
 
 server.start({
   cors:  {
