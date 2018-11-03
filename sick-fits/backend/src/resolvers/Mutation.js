ac1838e2c727ab931f9ae5e2eb83f9cca20bf0d7
Michael Flis <michaelflis07@gmail.com>
2018-11-03 13:00:32 -0700

    adds password reset backend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -119,8 +119,52 @@ const Mutations = {
     
     // ctx.response.clearCookie('token');
     // return { message: 'Goodbye.'}
-  }
+  },
+
+  async resetPassword(parent, args, ctx, info) {
+    // 1. check if passwords match
+    if ( args.password !== args.confirmPassword ) {
+      throw new Error('Your passwords do not match');
+    }
     
+    // 2. check if its a  legit reset token
+    // 3. check if its expired
+    const [ user ] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 3600000
+       }
+    })
+
+    if ( !user ) {
+      throw new Error('This token is either invalid or expired');
+    }
+
+    // 4. hash their new password
+    const password = await bcrypt.hash(args.password, 10);
+
+    // 5. save the new password to the user and remove old reset token fields
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: { email: user.email },
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null
+      }
+    })
+     
+    // 6. generate JWT
+    const token = jwt.sign({ userId: updatedUser.id }, process.env.APP_SECRET)
+
+    // 7. set JWT cookie
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    
+    // 8. return new user
+    return updatedUser;
+  }
 };
 
 module.exports = Mutations;
