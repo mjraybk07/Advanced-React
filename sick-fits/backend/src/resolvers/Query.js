6a18a40edd812006617760bf1f4dc402d658e36b
Michael Flis <michaelflis07@gmail.com>
2018-11-09 15:14:35 -0800

    adds permissions, begins permissions table build

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,4 +1,5 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
   items: forwardTo('db'),
@@ -15,11 +16,18 @@ const Query = {
       }, 
       info
     );
+  },
+  async users(parent, args, ctx, info) {
+   // 1. check if the user is logged in
+   if ( !ctx.request.userId ) {
+     throw new Error('You must be logged in!')
+   }
+   // 2. check if the user has the permissions to query all the users
+   hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
+
+   // 3. if they do, query all the users
+   return ctx.db.query.users({}, info);
   }
-  // async items(parent, args, ctx, info) {
-  //   const items = await ctx.db.query.items()
-  //   return items;
-  // }
 };
 
 module.exports = Query;
