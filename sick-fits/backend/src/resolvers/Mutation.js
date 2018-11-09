808b9dc8b02fffdfbbdd84f44f5525c5eee09b11
Michael Flis <michaelflis07@gmail.com>
2018-11-09 10:25:11 -0800

    adds user relationship to item creation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -8,13 +8,27 @@ const { transport, makeANiceEmail }  = require('../mail');
 const Mutations = {
   async createItem(parent, args, ctx, info){
     // TODO check if they are logged in
+    if ( !ctx.request.userId ) { 
+      throw new Error('You must be logged in to create a new item.  ')
+    }
     
-    const item = await ctx.db.mutation.createItem({
-      data: {
-        ...args
-      }
-    }, info);
-    console.log(item)
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          // This is how to create a relationship between the Item and the User
+          user: {
+            connect: {
+              id: ctx.request.userId,
+            },
+          },
+          ...args,
+        },
+      },
+      info
+    );
+
+    console.log(item);
+
     return item;
   },
 
