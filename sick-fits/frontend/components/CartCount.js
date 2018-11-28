16f7d518e5e0480190ad1b63a7297e8ac5da0d36
Michael Flis <michaelflis07@gmail.com>
2018-11-27 21:58:56 -0800

    adds cart count css animation

--- sick-fits/frontend/components/CartCount.js
+++ sick-fits/frontend/components/CartCount.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+
+const AnimationStyles = styled.span`
+  position: relative;
+  .count {
+    display: block;
+    position: relative;
+    transition: all 0.4s;
+    backface-visibility: hidden;
+  }
+  /* Initial State of the entered Dot */
+  .count-enter {
+    transform: scale(4) rotateX(0.5turn);
+  }
+  .count-enter-active {
+    transform: rotateX(0);
+  }
+  .count-exit {
+    top: 0;
+    position: absolute;
+    transform: rotateX(0);
+  }
+  .count-exit-active {
+    transform: scale(4) rotateX(0.5turn);
+  }
+`;
 
 const Dot = styled.div`
   background: ${props=> props.theme.red};
@@ -15,8 +41,21 @@ const Dot = styled.div`
   font-variant-numeric: 'tabular-nums';
 `;
 
+
 const CartCount =  ({ count }) => (
-  <Dot>{count}</Dot>
+  <AnimationStyles>
+    <TransitionGroup>
+      <CSSTransition 
+        unmountOnExit 
+        className="count" 
+        classNames="count" 
+        key={count}
+        timeout={{ enter: 400, exit: 400 }}
+      >
+        <Dot>{count}</Dot>
+      </CSSTransition>
+    </TransitionGroup>
+  </AnimationStyles>
 );
 
 export default CartCount;
